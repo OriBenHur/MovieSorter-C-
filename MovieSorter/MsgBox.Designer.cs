c2fa3975295991f70d894e246d3150fa2e88ce84
oribenhur <oribenhur@gmail.com>
2016-12-28 23:16:36 +0200

    Added Custom MsgBox

---
+++ MovieSorter/MsgBox.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace MovieSorter
+{
+    partial class MsgBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.OK = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.OK, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(81, 46);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // OK
+            // 
+            this.OK.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.OK.Location = new System.Drawing.Point(3, 20);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(75, 23);
+            this.OK.TabIndex = 0;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(75, 1);
+            this.panel1.TabIndex = 1;
+            // 
+            // MsgBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(86, 41);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MsgBox";
+            this.Text = "MsgBox";
+            this.Load += new System.EventHandler(this.MsgBox_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ MovieSorter/MsgBox.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MovieSorter
+{
+    public partial class MsgBox : Form
+    {
+        public MsgBox()
+        {
+            InitializeComponent();
+        }
+        public void AddLabeles(string lable, int x, int y )
+        {
+            LinkLabel _tmpLable = new LinkLabel();
+            RadioButton _checkBox = new RadioButton();
+            var location = new Point(x, y);
+            var checkBoxLocation = new Point(x - 15, y-5);
+            _checkBox.Location = checkBoxLocation;
+            _tmpLable.Text = lable;
+            _tmpLable.AutoSize = true;
+            _tmpLable.Location = location;
+
+            panel1.Controls.Add(_tmpLable);
+            panel1.Controls.Add(_checkBox);
+            _tmpLable.Click += link_Click;
+            _checkBox
+
+
+        }
+        
+        protected void link_Click(object sender, EventArgs e)
+        {
+            var link = (LinkLabel) sender;
+            Process.Start(link.Text);
+        }
+        private void OK_Click(object sender, EventArgs e)
+        {
+            var radio = (RadioButton) sender;
+            if (radio.Checked)
+            {
+                MessageBox.Show("ddd");
+            }
+        }
+
+        private void MsgBox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Checkbox_Chenge(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                var file = ((RadioButton)sender).Tag.ToString();
+            }
+        }
+    }
+}
